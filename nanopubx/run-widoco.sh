54d896fdb0cf90b3ef94ef4e444dbbd910d2de3b
Tobias Kuhn <kuhntobias@gmail.com>
2019-05-06 14:48:37 +0200

    Add Widoco scripts

---
+++ nanopubx/run-widoco.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+java -jar widoco-*-jar-with-dependencies.jar -confFile config/config.properties -ontFile ../nanopubx.ttl -outFolder .
---
+++ widoco/get-widoco.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+wget https://github.com/dgarijo/Widoco/releases/download/v1.4.10/widoco-1.4.10-jar-with-dependencies.jar
